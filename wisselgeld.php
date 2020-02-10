9071160d2fccda373f869f4ef7204dcd9c4c2eb3
Justin Plein <2099449@talnet.nl>
2020-02-10 17:40:31 +0100

    Uitwerking van de exercise

--- wisselgeld.php
+++ wisselgeld.php
@@ -7,22 +7,22 @@ while ($change) {
     if (floor($change / 10)) {
         $aantal10 = floor($change / 10);
         $change = $change % 10;
-        echo $aantal10 ." x ". 10 .PHP_EOL;
+        echo $aantal10 ." x ". 10 . " euro" .PHP_EOL;
 
     } else if (floor($change / 5)) {
         $aantal5 = floor($change / 5);
         $change = $change % 5;
-        echo $aantal5 ." x ". 5 .PHP_EOL;
+        echo $aantal5 ." x ". 5 . " euro".PHP_EOL;
 
     } else if (floor($change / 2)) {
         $aantal2 = floor($change / 2);
         $change = $change % 2;
-        echo $aantal2 ." x ". 2 .PHP_EOL;
+        echo $aantal2 ." x ". 2 . " euro".PHP_EOL;
 
     } else if (floor($change / 1)) {
         $aantal1 = floor($change / 1);
         $change = $change % 1;
-        echo $aantal1 ." x ". 1;
+        echo $aantal1 ." x ". 1 . " euro";
     }
 }
 ?>
